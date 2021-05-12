70459ff192a31358ce599ccf5d3f6c0c4676e54e
aldimhr <dirikuin16@gmail.com>
2021-05-12 16:28:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+   purge: [],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+      extend: {
+         colors: {
+            xgray: '#1c1c1c',
+            red: '#AD014E',
+            yellow: '#fcb54b',
+            blue: '#d3e1dd',
+         },
+
+         fontFamily: {
+            montserrat: 'Montserrat, sans-serif',
+            gotham: 'Gotham',
+         },
+
+         spacing: {
+            500: '500px',
+         },
+      },
+   },
+   variants: {
+      extend: {},
+   },
+   plugins: [],
+};
